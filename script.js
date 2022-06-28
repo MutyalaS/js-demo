8a8d292dac7c23877e6bbd658b53a6c5d2824cf2
surekha <surekhamutyala.qa@gmail.com>
2022-06-27 18:27:58 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const name ='Surekah Mutyala'
+console.log('name:', name)
+const station =''
+console.log(typeof station)
+
+const age = 32.5
+console.log(typeof age)
+
+const isCanadian = false
+console.log('isCandian:' , isCanadian)
+
+const hairColor = undefined
+console.log(typeof hairColor)
+
+// Null
+
+const feather =null
+console.log(typeof feather)
+
+// Non primitive datatypes
+
+// Objects
+// Array 
+
+// Undefined
+
+// boolean true or false
